8928ea1fe92c93c057ff1f14ce1416898c22511f
Masaki Murase <murasemasaki_0216@yahoo.co.jp>
2015-01-08 21:17:22 +0900

    各部屋のコメント投稿フォームの作成

--- app/controllers/room_comments_controller.rb
+++ app/controllers/room_comments_controller.rb
@@ -7,7 +7,26 @@ class RoomCommentsController < ApplicationController
 
 	#指定された部屋のコメントを表示する
 	def show
-		@comments = RoomComment.where(room_id: params[:id]).order(offset: :asc)
+		@comments = RoomComment.where(room_id: params[:id]).order(id: :desc)
+	end
+
+	# コメントの投稿
+	# POST /room_comments
+	def create
+		@comment = RoomComment.new(roomComments_params)
+		puts "コメント投稿"
+		p @comment
+		@comment.save
+		redirect_to room_comment_path(@comment[:room_id])
+	end
+
+
+	private
+	# roomsテーブルにある項目だけを取り出す
+	def roomComments_params
+		puts "プライベートメソッドroomComments_params"
+		p params
+		params.require(:room_comment).permit(:comment, :room_id)
 	end
 
 end
