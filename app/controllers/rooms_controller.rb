fc473eab72e0eaae2c88bb2d1a7dad25ef69b8fe
Masaki Murase <murasemasaki_0216@yahoo.co.jp>
2015-01-08 17:25:04 +0900

    部屋作成フォームの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,27 @@
 class RoomsController < ApplicationController
-	
+
 	# 最初の画面, すべての部屋を表示する
+	# GET /rooms
 	def index
 		@rooms = Room.all
 	end
+
+	# 新規部屋作成
+	# POST /rooms
+	def create
+		@room = Room.new(rooms_params)
+#		puts "roomの中身"
+#		p @room
+		@room.save
+		redirect_to rooms_path
+	end
+
+	private
+	# roomsテーブルにある項目だけを取り出す
+	def rooms_params
+#		p params
+		params.require(:room).permit(:name)
+	end
+
+
 end
